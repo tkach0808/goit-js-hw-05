d30c78854c92e784bb3d9aead6941323c492a884
tkach0808 <tkach0808@gmail.com>
2020-05-17 14:47:15 +0300

    HW #5, student:Tkach

---
+++ js/task-03.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Storage {
+  constructor(array) {
+    this.items = array;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    this.items.splice(this.items.includes(item), 1);
+  }
+}
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
---
+++ js/task-05.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* eslint-disable */
+class Car {
+  constructor({ speed, price, maxSpeed, isOn, distance = 0 }) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  static getSpecs(car) {
+    this.speed = car.speed;
+    this._price = car.price;
+    this.maxSpeed = car.maxSpeed;
+    this.isOn = car.isOn;
+    this.distance = car.distance;
+    console.log(
+      `maxSpeed = ${this.maxSpeed}, speed = ${this.speed}, isOn = ${this.isOn}, distance = ${this.distance}, price = ${this._price}`,
+    );
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+
+  accelerate(value) {
+    if (value <= this.maxSpeed) {
+      this.speed = value;
+    }
+  }
+
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed = this.speed - value;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
